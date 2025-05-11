c0c25b0ed9ee8e7eb18cc409b076e898af03e17c
MFarisA <muhfarisassami@gmail.com>
2025-05-11 22:19:19 +0700

    Add Rooms and RoomsParty components with placeholder content

---
+++ app/room/page.tsx
@@ -0,0 +1,7 @@
+export default function Rooms() {
+    return (
+        <div>
+            <p>kingkangkung</p>
+        </div>
+    );
+}
---
+++ app/roomsParty/page.tsx
@@ -0,0 +1,7 @@
+export default function RoomsParty() {
+    return (
+        <div>
+            <p>kungkingkang</p>
+        </div>
+    );
+}
